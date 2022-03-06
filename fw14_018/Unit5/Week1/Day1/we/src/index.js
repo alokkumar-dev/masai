470f344b5328ee5dd96399ebb4fc7ade990d6609
alokkumar-dev <alokshakya2601@gmail.com>
2022-03-06 11:47:38 +0530

    mofify files

--- fw14_018/Unit5/Week1/Day1/we/src/index.js
+++ fw14_018/Unit5/Week1/Day1/we/src/index.js
@@ -4,4 +4,19 @@ const h1 = document.createElement("h1");
 h1.innerText = "Hello Alok kumar!";
 h1.classList = "redtext";
 
-document.getElementById("root").append(h1);
+const input = document.createElement("input");
+input.classList = "inputfield";
+const button = document.createElement("button");
+const div = document.createElement("div");
+
+button.innerText = "Submit";
+let p = document.createElement("p");
+button.addEventListener("click", () => {
+  let value = input.value;
+  p.innerText = value;
+
+});
+
+div.append(input,button,p)
+
+document.getElementById("root").append(h1, div);
