501744908469d84b19ddfcc3be13ba23eb8bf749
alokkumar-dev <alokshakya2601@gmail.com>
2022-03-11 18:51:45 +0530

    modify files

--- fw14_018/Unit5/Week2/Day4/assignment/react-forms/src/App.jsx
+++ fw14_018/Unit5/Week2/Day4/assignment/react-forms/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import { Forms } from './components/Forms'
 
 function App() {
 
   return (
-    <div className="App">
-        <h1>React Forms</h1>
+    <div>
+        <h1 className="Center">React Forms</h1>
         <Forms />
     </div>
   )
--- fw14_018/Unit5/Week2/Day4/assignment/react-forms/src/components/Forms.jsx
+++ fw14_018/Unit5/Week2/Day4/assignment/react-forms/src/components/Forms.jsx
@@ -1,92 +1,103 @@
+import "../App.css"
 import axios from "axios";
 import { useState } from "react"
 import { useEffect } from "react";
 
-export const Forms = ()=>{
+export const Forms = () => {
 
     const [formData, setFormData] = useState({});
     const [data, setData] = useState([]);
 
 
-    const handleChange = (e)=>{
-        let {id,value, checked,type} = e.target
-        value=type==="checkbox" ? checked : value;
+    const handleChange = (e) => {
+        let { id, value, checked, type } = e.target
+        value = type === "checkbox" ? checked : value;
         setFormData({
-            ...formData, 
+            ...formData,
             [id]: value
         })
     }
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post("http://localhost:3001/users", formData).then(()=>{
+        axios.post("http://localhost:3001/users", formData).then(() => {
             alert("User Created Successfully");
             setFormData();
         })
+        
     }
-   
-    const getFormData = ()=>{
-        axios.get("http://localhost:3001/users").then((res)=>{
+
+    const getFormData = () => {
+        axios.get("http://localhost:3001/users").then((res) => {
             setData(res.data);
         })
     }
-    useEffect(()=>{
+    useEffect(() => {
         getFormData();
 
-    },[formData]);
+    }, [formData]);
 
     return (
         <>
-        <div>
-            <form action="" onSubmit={handleSubmit}>
-            <input type="text" id="name" onChange={handleChange}   placeholder="enter your name" /><br />
-            <input type="number" id="age" onChange={handleChange} placeholder="enter your age" /><br />
-            <input type="text" id="address" onChange={handleChange} placeholder="enter your address" /><br />
-                <input type="number" id="salary" onChange={handleChange} placeholder="entre your salary" /><br />
+            <div className="MainDiv">
+                <form action="" onSubmit={handleSubmit}>
+                    <label htmlFor="name">Name:</label><br />
+                    <input type="text" id="name" onChange={handleChange} placeholder="enter your name" required /><br />
 
-            <select name="" id="department" onChange={handleChange}>
-                <option value="">Select</option>
-                <option value="IT">IT</option>
-                <option value="Civil">Civil</option>
-                <option value="Mechnical">Mechnical</option>
-                <option value="Eletrical">Electrical</option>
-            </select>
-            <input type="checkbox" id="married" onChange={handleChange} />
-                <input type="submit" value={"Submit"} />
+                    <label htmlFor="age">Age:</label><br />
+                    <input type="number" id="age" onChange={handleChange} placeholder="enter your age" required /><br />
 
-            </form>
-        
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Address</th>
-                    <th>Salary</th>
-                    <th>Department</th>
-                    <th>Marital Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map((e)=>{
-                        return (
-                            <tr key={e.id}>
-                                <td>{e.name}</td>
-                                <td>{e.age}</td>
-                                <td>{e.address}</td>
-                                <td>{e.salary}</td>
-                                <td>{e.department}</td>
-                                <td>{e.married ? "true" : "false"}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
-        </div>
-         </>
-       
+                    <label htmlFor="address">Address:</label><br />
+                    <input type="text" id="address" onChange={handleChange} placeholder="enter your address"required /><br />
+
+                    <label htmlFor="salary">Salary:</label><br />
+                    <input type="number" id="salary" onChange={handleChange} placeholder="entre your salary" required /><br />
+
+                    <label htmlFor="department">Name:</label><br />
+                    <select name="" id="department" onChange={handleChange}>
+                        <option value="">Select</option>
+                        <option value="IT">IT</option>
+                        <option value="Civil">Civil</option>
+                        <option value="Mechnical">Mechnical</option>
+                        <option value="Eletrical">Electrical</option>
+                    </select><br />
+
+                    <label htmlFor="married">Marital Status:</label>                 
+                    <input type="checkbox" id="married" onChange={handleChange} /><br />
+                    <input type="submit" id="submit" value={"Submit"} />
+
+                </form>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Address</th>
+                            <th>Salary</th>
+                            <th>Department</th>
+                            <th>Marital Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((e) => {
+                                return (
+                                    <tr key={e.id}>
+                                        <td>{e.name}</td>
+                                        <td>{e.age}</td>
+                                        <td>{e.address}</td>
+                                        <td>{e.salary}</td>
+                                        <td>{e.department}</td>
+                                        <td>{e.married ? "true" : "false"}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
+
     )
 }
-   
