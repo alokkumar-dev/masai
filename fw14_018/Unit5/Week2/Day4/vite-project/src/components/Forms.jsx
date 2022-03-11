0f140e8dfe810a73a5d318b66012da18fc48fc58
alokkumar-dev <alokshakya2601@gmail.com>
2022-03-11 18:59:30 +0530

    modify fils

--- fw14_018/Unit5/Week2/Day4/vite-project/src/components/Forms.jsx
+++ fw14_018/Unit5/Week2/Day4/vite-project/src/components/Forms.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { useState } from "react"
+import { ShowFormData } from "./ShowFormData";
 
 export const Forms = () => {
+    const [data,setData]= useState([]);
+    // console.log(data)
     const [formData, setFormData] = useState({
         username: "",
         email: "",
         age: ""
 
     });
+    console.log(formData.username)
 
     const handleChange = (e) => {
         console.log(e.target.id, e.target.value);
@@ -27,15 +31,35 @@ export const Forms = () => {
                 email: "",
                 age: ""
             })
+        }).then(()=>{
+            getData()
         })
     }
+    
+    const getData = ()=>{
+        axios.get("http://localhost:3001/users").then(res=>{
+            
+            console.log(res.data)
+            setData(res.data);
+        })
+    }
+    // getData()
+  
     return (
-        <form action="" onSubmit={handleSubmit}>
-            <h3>SignUp</h3>
-            <input type="text" value={formData.username} id="username" onChange={handleChange} placeholder="enter username" />
-            <input type="email" value={formData.email} id="email" onChange={handleChange} placeholder="enter emal" />
-            <input type="number" value={formData.age} id="age" onChange={handleChange} placeholder="enter age" />
-            <input type="submit"  value={"create user"} />
-        </form>
+        <>
+            <form action="" onSubmit={handleSubmit}>
+                <h3>SignUp</h3>
+                <input type="text" value={formData.username} id="username" onChange={handleChange} placeholder="enter username" />
+                <input type="email" value={formData.email} id="email" onChange={handleChange} placeholder="enter emal" />
+                <input type="number" value={formData.age} id="age" onChange={handleChange} placeholder="enter age" />
+                <input type="submit"  value={"create user"} />
+            </form>
+            {
+                data.map((e)=>{
+                    console.log(e.username);
+                    <ShowFormData username={e.username} email={e.email} age={e.age} />
+                })
+            }
+        </>
     )
 } 
---
+++ fw14_018/Unit5/Week2/Day4/vite-project/src/components/ShowFormData.jsx
@@ -0,0 +1,11 @@
+export const ShowFormData = ({username,email, age})=>{
+    return (
+        <>
+            <div>
+                <span>{username}</span>
+                <span>{email}</span>
+                <span>{age}</span>
+            </div>
+        </>
+    )
+}
