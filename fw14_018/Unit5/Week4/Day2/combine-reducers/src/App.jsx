47059c051b1309c556cfee6983cffca0c7eaaf68
alokkumar-dev <alokshakya2601@gmail.com>
2022-03-24 14:14:11 +0530

    modify files

---
+++ fw14_018/Unit5/Week4/Day2/combine-reducers/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleCount = (value)=>{
+    setCount(count+value);
+  } 
+
+  return (
+    <div className="App">
+     <h1>Hello React Counter: {count} </h1>
+     <button onClick={()=>{handleCount(1)}}>Add 1</button>
+    </div>
+  )
+}
+
+export default App
