0148825ab216b5389902aa784b64afaf88de935b
alokkumar-dev <alokshakya2601@gmail.com>
2022-04-08 17:42:59 +0530

    add infinity schrolling whole code

---
+++ fw14_018/infinity-schrolling-js/index.js
@@ -0,0 +1,21 @@
+let container = document.querySelector(".container");
+let count = 1;
+const displayData = ()=>{
+    let page = 1;
+    while(page <= 25){
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        p.innerText = `Masai School ${count}`;
+        div.append(p);
+        container.append(div);
+        count++;
+        page++;
+    }
+}
+displayData();
+container.addEventListener("scroll", ()=>{
+    if(container.scrollHeight - container.scrollTop - container.clientHeight<1){
+        displayData();
+        console.log("Reached");
+    }
+})
