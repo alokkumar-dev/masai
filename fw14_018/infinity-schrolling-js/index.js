c24ebba6f5297dc319db37af21d249acdd29f8c4
alokkumar-dev <alokshakya2601@gmail.com>
2022-04-08 19:27:02 +0530

    modify files

--- fw14_018/infinity-schrolling-js/index.js
+++ fw14_018/infinity-schrolling-js/index.js
@@ -1,21 +1,26 @@
 let container = document.querySelector(".container");
 let count = 1;
-const displayData = ()=>{
-    let page = 1;
-    while(page <= 25){
-        let div = document.createElement("div");
-        let p = document.createElement("p");
-        p.innerText = `Masai School ${count}`;
-        div.append(p);
-        container.append(div);
-        count++;
-        page++;
-    }
-}
+const displayData = () => {
+  let page = 1;
+  while (page <= 25) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.innerText = `Masai School ${count}`;
+    div.append(p);
+    container.append(div);
+    count++;
+    page++;
+  }
+};
 displayData();
-container.addEventListener("scroll", ()=>{
-    if(container.scrollHeight - container.scrollTop - container.clientHeight<1){
-        displayData();
-        console.log("Reached");
+container.addEventListener("scroll", () => {
+  setTimeout(() => {
+    if (
+      container.scrollHeight - container.scrollTop - container.clientHeight <
+      1
+    ) {
+      displayData();
+      console.log("Reached");
     }
-})
+  }, 500);
+});
